e3ab4217ebe13ab13ecdc531009e64e7b3d524f0
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-11 11:17:36 +0600

    fixed file issue

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
@@ -8,34 +9,35 @@ import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css';
 
 const Orders = () => {
-    const [products, setProducts] = useProducts();
-    const [cart, setCart] = useCart(products);
-    const navigate = useNavigate();
-    const handleRemoveProduct = product =>{
-        const rest = cart.filter(pd => pd.id !== product.id);
-        setCart(rest);
-        removeFromDb(product.id);
-    }
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
+  const navigate = useNavigate();
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+    removeFromDb(product.id);
+  };
 
-    
-    return (
-        <div className='shop-container'>
-            <div className="review-items-container">
-                {
-                    cart.map(product => <ReviewItem
-                        key={product.id}
-                        product ={product}
-                        handleRemoveProduct = {handleRemoveProduct}
-                    ></ReviewItem>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}>
-                        <button onClick={()=>navigate('/shipment')}>Proceed Shipping </button>
-                </Cart>
-            </div>
-        </div>
-    );
+  return (
+    <div className="shop-container">
+      <div className="review-items-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveProduct={handleRemoveProduct}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <button onClick={() => navigate('/shipment')}>
+            Proceed Shipping{' '}
+          </button>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
